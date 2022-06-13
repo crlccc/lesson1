b67bf196e6f9d0dd5b2751925487fe78a3752cfc
crlccc <www.crl9606@outlook.com>
2022-06-14 01:10:47 +0800

    变更

---
+++ src/main/java/com/example/lesson1/controller/demoController.java
@@ -0,0 +1,12 @@
+package com.example.lesson1.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class demoController {
+
+
+
+}
